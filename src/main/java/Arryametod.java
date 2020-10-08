56912955a7a3e7677aa5787fc1690f4f89ef0d4a
Igor <ishirayev@gmail.com>
2020-10-08 19:34:58 +0300

    Arraymetod

--- src/main/java/Arryametod.java
+++ src/main/java/Arryametod.java
@@ -3,30 +3,39 @@ import java.util.Arrays;
 
 public class Arryametod {
 
-   static Integer[] myArr = new Integer[]{1, 4, 3, 1, 5, 6, 7};
+   static Integer[] myArr = new Integer[]{1, 8, 3, 1, 5, 6, 7};
 
     public static Integer[] afterLasFour(Integer[] arr) {
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] == 4){
-//            break;
-//        }else {
-//              throw (Throwable) RuntimeException;
-//            }}
-
-
-        ArrayList intermediate = new ArrayList();
-        for (int i = arr.length - 1; i >= 0 ; i--) {
-            if (arr[i] != 4) {
-                intermediate.add(0, arr[i]);
-            } else {
-                break;
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 4) {
+                k++;
             }
         }
-         Integer[] afterLasFour = new Integer[intermediate.size()];
-         intermediate.toArray(afterLasFour);
-         return afterLasFour;
+         if (k == 0) {
+           try {
+               throw new RuntimeException("Array test");
+           } catch (RuntimeException e) {
+              System.out.println("Отсутствует 4 в входном массиве");
+           }
+       }
 
-    }
+            ArrayList intermediate = new ArrayList();
+            for (int i = arr.length - 1; i >= 0; i--) {
+                if (arr[i] != 4) {
+                    intermediate.add(0, arr[i]);
+                } else {
+                    break;
+                }
+            }
+            Integer[] afterLasFour = new Integer[intermediate.size()];
+            intermediate.toArray(afterLasFour);
+            return afterLasFour;
+
+        }
+
+
+
     static int[] myArrInt = new int[]{4, 4, 4, 4, 4, 4};
 
     public static boolean arrayСomposition(int[] arr) {
@@ -42,8 +51,8 @@ public class Arryametod {
 
 
     public static void main(String[] args) {
-        System.out.println(arrayСomposition(myArrInt));
-        //System.out.println(Arrays.toString(afterLasFour(myArr)));
+    //    System.out.println(arrayСomposition(myArrInt));
+        System.out.println(Arrays.toString(afterLasFour(myArr)));
     }
 
 
