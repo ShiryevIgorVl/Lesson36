5f1017a95f923f2945ef9a439b3ed1d68784e779
Igor <ishirayev@gmail.com>
2020-10-08 18:55:35 +0300

    Arraymetod

--- src/main/java/Arryametod.java
+++ src/main/java/Arryametod.java
@@ -3,9 +3,16 @@ import java.util.Arrays;
 
 public class Arryametod {
 
-    static int[] myArr = new int[]{1, 4, 3, 4, 5, 6, 7};
+   static Integer[] myArr = new Integer[]{1, 4, 3, 1, 5, 6, 7};
 
-    public static Integer[] afterLasFour(int[] arr){
+    public static Integer[] afterLasFour(Integer[] arr) {
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == 4){
+//            break;
+//        }else {
+//              throw (Throwable) RuntimeException;
+//            }}
+
 
         ArrayList intermediate = new ArrayList();
         for (int i = arr.length - 1; i >= 0 ; i--) {
@@ -20,11 +27,26 @@ public class Arryametod {
          return afterLasFour;
 
     }
+    static int[] myArrInt = new int[]{4, 4, 4, 4, 4, 4};
+
+    public static boolean arrayСomposition(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+             for (int j = 0; j < arr.length; j++) {
+                    if (arr[i] == 4 && arr[j] == 1) {
+                        return true;
+                    }
+                }
+            }
+        return false;
+        }
 
 
-    public static void main(String[] args)
-     {
-        System.out.println(Arrays.toString(afterLasFour(myArr)));
+    public static void main(String[] args) {
+        System.out.println(arrayСomposition(myArrInt));
+        //System.out.println(Arrays.toString(afterLasFour(myArr)));
     }
+
+
+
 
 }
--- src/test/java/Arryatest.java
+++ src/test/java/Arryatest.java
@@ -1,2 +1,51 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import sun.management.snmp.jvmmib.JVM_MANAGEMENT_MIBOidTable;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+
 public class Arryatest {
+
+    public Integer[] a;
+    public Integer[] b;
+
+    public Arryatest(Integer[] a, Integer[] b) {
+        this.a = a;
+        this.b = b;
+    }
+
+
+    Arryametod arryametod;
+
+   int[] myArr = new int[]{};
+
+    @Parameterized.Parameters
+    public Collection<Object[]> data () {
+        return Arrays.asList(new Object[][]{
+                {new Integer[]{5, 6, 7}, new Integer[]{1, 4, 3, 4, 5, 6, 7}}
+        });
+
+    }
+
+    @Before
+        public void init() {
+            arryametod = new Arryametod();
+        }
+
+
+
+@Test
+    public void afterLasFourTest(){
+     Assert.assertArrayEquals(a, arryametod.afterLasFour(b));
+    }
+
+
 }
